34f301d1cda5739e779fe9ca0497112b292e4fce
Osvaldo <60455596+osvaldo-o@users.noreply.github.com>
2020-11-21 08:51:36 -0600

    Add files via upload

--- Tarea 7/pruebas.py
+++ Tarea 7/pruebas.py
@@ -1,18 +1,23 @@
 from ListaDoblementeLigada import DoubleLinkedList
 
-x = DoubleLinkedList()
-x.append(10)
-x.append(20)
-x.append(30)
-x.append(40)
-x.transversal()
-print(x.find_from_tail(20))
-print(x.find_from_head(20))
-x.reverse_transversal()
-x.remove_from_tail(10)
-x.transversal()
-x.append(50)
-x.transversal()
-x.remove_from_head(50)
-x.transversal()
-print(x.get_size())
+l = DoubleLinkedList()
+# test append
+l.append(10)
+l.append(20)
+l.append(30)
+l.append(40)
+l.append(50)
+l.append(55)
+# test transversal
+l.transversal()
+l.reverse_transversal()
+# test find_from
+print(f"Buscando a partir de head, la posición del valor 30 es: {l.find_from_head(30)}")
+print(f"Buscando a partir de tail, la posición del valor 20 es: {l.find_from_tail(20)}")
+# test remove
+l.remove_from_head(20)
+l.remove_from_tail(40)
+l.transversal()
+l.reverse_transversal()
+# test get size
+print(f"El tamaño de la lista es: {l.get_size()}")
